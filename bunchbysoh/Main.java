7bc6861d9aac10f07ac3ee98343ecefb67af3b4c
guptaawantika <136964885+guptaawantika@users.noreply.github.com>
2023-09-08 19:33:51 +0530

    Update Main.java
    Calculating soh for each present capacity of the battery and classifying them.

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -9,16 +9,40 @@ public class Main {
 
   static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
     CountsBySoH counts = new CountsBySoH();
+    int n = presentCapacities.length; // length of the array
+    int SoH[]=new int[n];
+    //iterating over the given present capacitites for calculating SoH and classifying batteries
+    for(int i=0;i<n;i++)                            
+    {
+        SoH[i]=100*presentCapacities[i]/120;   //assuming 120Ah as rated capacity
+        if(SoH[i]>80)
+        {
+            counts.healthy++;
+            
+        }
+        else if(SoH[i]<=80 && SoH[i]>=65)
+        {
+            counts.exchange++;
+        }
+        else{
+            counts.failed++;
+        }
+    }
     return counts;
   }
 
   static void testBucketingByHealth() {
     System.out.println("Counting batteries by SoH...\n");
-    int[] presentCapacities = {115, 118, 80, 95, 91, 77};
+    int[] presentCapacities = {115,118, 80, 95, 91, 77};
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
+    assert(counts.healthy+counts.exchange+counts.failed==presentCapacities.length);
+    //printing number of batteries of each type
+    System.out.println("Number of healthy batteries: "+counts.healthy);
+    System.out.println("Number of exchange batteries: "+counts.exchange);
+    System.out.println("Number of failed batteries: "+counts.failed);
     System.out.println("Done counting :)\n");
   }
 
